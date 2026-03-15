d2bc912749f2c0b69484c8013d638b3add285156
Mohamed Elmengah <101114397+MohamedElmengah@users.noreply.github.com>
2026-03-15 10:43:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string code = "int x; read x; x := x + 1; write x;";
+
+        string pattern = @"[A-Za-z]+|\d+|:=|[+\-*/]|;";
+
+        MatchCollection matches = Regex.Matches(code, pattern);
+
+        foreach (Match m in matches)
+        {
+            Console.WriteLine(m.Value);
+        }
+    }
+}
